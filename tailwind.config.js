8dcc05d73c2bdfa3546302d039f12924ec371867
kevinusmayadhyw <57991938+kevinusmayadhyw@users.noreply.github.com>
2022-05-18 20:49:41 +0700

    added css and html code

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'pacifico' : ['Pacifico'],
+        'amiri' : ['Roboto Condensed']
+      },
+      screens: {
+        '3xl': '2000px'
+      },
+    },
+  },
+  plugins: [],
+}
